a651bc0591725a2c3a0d739d5439c2676fc152ec
Kenneth Heiserman <kennethheiserman@yahoo.com>
2023-04-17 18:50:55 -0500

    added BarcodeScanning.kt to git

---
+++ app/src/main/java/com/example/kitchenplanner/BarcodeScanning.kt
@@ -0,0 +1,11 @@
+package com.example.kitchenplanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BarcodeScanning : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_barcode_scanning)
+    }
+}
