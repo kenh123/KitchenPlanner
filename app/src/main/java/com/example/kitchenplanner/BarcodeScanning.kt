4b60c1e51e7550c273599179ee3946600d0d6e2d
Kenneth Heiserman <kennethheiserman@yahoo.com>
2023-04-22 14:13:52 -0500

    added barcode scanner to BarcodeScanning

--- app/src/main/java/com/example/kitchenplanner/BarcodeScanning.kt
+++ app/src/main/java/com/example/kitchenplanner/BarcodeScanning.kt
@@ -3,14 +3,33 @@ package com.example.kitchenplanner
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.kitchenplanner.databinding.ActivityMainBinding
+import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
 
 class BarcodeScanning : AppCompatActivity() {
 
     private lateinit var viewBinding: ActivityMainBinding
+
+    private val options = GmsBarcodeScannerOptions.Builder().build()
+
+    private val scanner = GmsBarcodeScanning.getClient(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
         //setContentView(R.layout.activity_barcode_scanning)
+        //TODO figure out what to put in these parts
+        scanner.startScan()
+            .addOnSuccessListener {barcode ->
+
+            }
+            .addOnCanceledListener {
+
+            }
+            .addOnFailureListener{e ->
+
+            }
+
+        //val rawValue: String? = barcode.rawValue
     }
 }
